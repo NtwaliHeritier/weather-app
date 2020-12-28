bdc435b8437628778c1f2eac0e22d8b9c0d5f5ac
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-28 23:38:54 +0200

    add displaying image according to location

--- src/apicall.js
+++ src/apicall.js
@@ -3,7 +3,7 @@ const Api = (() => {
 		const form = document.forms['api-call'];
 		let unit = '';
 		const toggle = document.querySelector('.toggle-btn');
-		toggle.addEventListener('click', async () => {
+		toggle.addEventListener('click', () => {
 			if (toggle.checked) {
 				unit = 'imperial';
 			} else {
@@ -15,12 +15,20 @@ const Api = (() => {
 		form.addEventListener('submit', async (e) => {
 			e.preventDefault();
 			processData('metric');
+			try {
+				const response = await getImage(document.querySelector('input').value);
+				const container = document.querySelector('.container');
+				container.style.background = `url(${response.results[0].urls.regular})`;
+				container.style.backgroundRepeat = 'no-repeat';
+				container.style.backgroundSize = 'cover';
+			} catch (e) {
+				console.log(e);
+			}
 		});
 	};
 
 	const getData = async (location, unit) => {
 		const query = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=94193b62c5095be38288793639b5cfdc`;
-		console.log(query);
 		const data = await fetch(query);
 		const response = await data.json();
 		return response;
@@ -31,12 +39,20 @@ const Api = (() => {
 		try {
 			const response = await getData(location, unit);
 			const result = document.querySelector('.result');
-			result.textContent = response.main.temp;
+			result.textContent = response.main.temp + ' deg.';
 			result.textContent += unit == 'metric' ? ' Celcius' : ' Fahrenheit';
 			result.innerHTML += `<br> ${response.weather[0].description}`;
 		} catch (e) {
 			console.log(e.message);
 		}
+	};
+
+	const getImage = async (location) => {
+		const query = `https://api.unsplash.com/search/photos?page=1&query="${location} landscape"&orientation=landscape&per_page=1&client_id=_Icg2UQ8IkX1mbHiRXvGgX5by5rfy13ncjpH--5-DX8`;
+		console.log(query);
+		const data = await fetch(query);
+		const response = await data.json();
+		return response;
 	};
 
 	return { apicall };
