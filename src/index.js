cfc84710c78236d91972d4e0d1114bbc5912fa80
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-28 17:53:53 +0200

    add toggle button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import Api from './apicall'
+import Api from './apicall';
+import './index.css';
 Api.apicall();
+
+const toggle = document.querySelector('.toggle-btn');
+toggle.addEventListener('click', () => {
+	if (toggle.checked) {
+		console.log('checked');
+	} else {
+		console.log('unchecked');
+	}
+});
