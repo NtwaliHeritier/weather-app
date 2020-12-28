82ae2a70192cc2448925ec99fa158c49e8eeebd3
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-28 21:34:37 +0200

    add calls to front end

--- src/apicall.js
+++ src/apicall.js
@@ -1,26 +1,44 @@
 const Api = (() => {
 	const apicall = () => {
 		const form = document.forms['api-call'];
+		let unit = '';
+		const toggle = document.querySelector('.toggle-btn');
+		toggle.addEventListener('click', async () => {
+			if (toggle.checked) {
+				unit = 'imperial';
+			} else {
+				unit = 'metric';
+			}
+			processData(unit);
+		});
+
 		form.addEventListener('submit', async (e) => {
 			e.preventDefault();
-			const location = document.querySelector('input').value;
-			try {
-				const response = await getData(location);
-				const result = document.querySelector('.result');
-				result.textContent = response.main.temp;
-			} catch (e) {
-				console.log(e.message);
-			}
+			processData('metric');
 		});
 	};
 
-	const getData = async (location) => {
-		const query = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=94193b62c5095be38288793639b5cfdc`;
+	const getData = async (location, unit) => {
+		const query = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=94193b62c5095be38288793639b5cfdc`;
 		console.log(query);
 		const data = await fetch(query);
 		const response = await data.json();
 		return response;
 	};
+
+	const processData = async (unit) => {
+		const location = document.querySelector('input').value;
+		try {
+			const response = await getData(location, unit);
+			const result = document.querySelector('.result');
+			result.textContent = response.main.temp;
+			result.textContent += unit == 'metric' ? ' Celcius' : ' Fahrenheit';
+			result.innerHTML += `<br> ${response.weather[0].description}`;
+		} catch (e) {
+			console.log(e.message);
+		}
+	};
+
 	return { apicall };
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import Api from './apicall';
 import './index.css';
 Api.apicall();
-
-const toggle = document.querySelector('.toggle-btn');
-toggle.addEventListener('click', () => {
-	if (toggle.checked) {
-		console.log('checked');
-	} else {
-		console.log('unchecked');
-	}
-});
